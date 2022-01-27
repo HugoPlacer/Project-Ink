4fcef7230179ecfd0ed1a6597ccdd296badbd3ac
HugoPlacer <hugoplanellmoreno@gmail.com>
2022-01-27 16:51:12 +0100

    Text Dialog system

--- editvalues.gd
+++ editvalues.gd
@@ -2,15 +2,22 @@ extends Panel
 
 onready var controlnode = get_parent()
 
+onready var diastotales = $VBoxContainer/HBoxContainer/DiasTotales
+onready var tintatotal = $"VBoxContainer/HBoxContainer2/Tinta Total2"
 onready var medicion = $VBoxContainer/HBoxContainer3/LineEdit
+
 onready var edit_popup = $"Editar valores"
 
 onready var blur = $"../Blur"
 
+var index
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	medicion.text = str(controlnode.ink_used)
+	diastotales.text = str(controlnode.days_used)
+	tintatotal.text = str(controlnode.ink_total)
+	#medicion.text = str(controlnode.ink_used)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,14 +36,49 @@ func _on_hide_pressed():
 
 
 func _on_enviar_pressed():
-	controlnode.ink_used = float(medicion.text)
+	pass
+	#controlnode.ink_used = float(medicion.text)
 
 
+func _on_DiasTotales_focus_entered():
+	index = 0
+	edit_popup.popup_centered()
 
 
-func _on_LineEdit_focus_entered():
-	edit_popup.popup()
+func _on_Tinta_Total2_focus_entered():
+	index = 1
+	edit_popup.popup_centered()
 
 
-func _on_Button2_pressed():
+func _on_Medicion_tinta_focus_entered():
+	index = 2
+	edit_popup.popup_centered()
+
+
+func _on_Cancel_pressed():
+	if edit_popup.is_visible():
+		edit_popup.hide()
+
+
+func _on_Accept_pressed():
+	
+	var text = float($"Editar valores/VBoxContainer/LineEdit".text)
+	
+	if index == 0:
+		controlnode.days_used = text
+		diastotales.text = str(text)
+	
+	elif index == 1:
+		controlnode.ink_total = text
+		tintatotal.text = str(text)
+	
+		
+	elif index == 2:
+		controlnode.ink_used = text
+		medicion.text = str(text)
+	
 	edit_popup.hide()
+	
+
+
+
